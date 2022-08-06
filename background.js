e70278cb13f0ddc0305bb1583f5af188e6046b09
Digs.fm <digsfm@protonmail.com>
2022-08-07 01:20:00 +0300

    Migrate to Manifest v3 for Chrome

--- background.js
+++ background.js
@@ -21,14 +21,25 @@ async function wantToListen(tab) {
     icon = iconError;
   }
 
-  chrome.pageAction.setIcon({path: icon, tabId: tab.id});
+  chrome.action.setIcon({path: icon, tabId: tab.id});
 }
 
 function showAction(tabId, changeInfo, tab) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.pageAction.show(tabs[0].id);
+    let tab = tabs[0];
+    let url = tab.url;
+
+    if (url.startsWith('https://www.discogs.com/master/*') ||
+      url.startsWith('https://www.discogs.com/release/*')  ||
+      url.startsWith('https://open.spotify.com/album/*')   ||
+      url.startsWith('https://www.mixcloud.com/*/*')       ||
+      url.startsWith('https://*.bandcamp.com/album/*')) {
+      chrome.pageAction.show(tabs.id);
+    } else {
+      chrome.pageAction.hide(tabs.id);
+    }
   });
 };
 
 chrome.tabs.onUpdated.addListener(showAction);
-chrome.pageAction.onClicked.addListener(wantToListen);
+chrome.action.onClicked.addListener(wantToListen);
